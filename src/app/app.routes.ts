140ce23929be8d73bf06f5d596a0b36ac41269d2
MassimoC-137 <massimo.caramanna@gmail.com>
2024-07-08 15:17:48 +0200

    creazione component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { CarListComponent } from './car-list/car-list.component';
+import { CarDetailComponent } from './car-detail/car-detail.component';
+import { BookingListComponent } from './booking-list/booking-list.component';
+import { BookingDetailComponent } from './booking-detail/booking-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'users', component: UserListComponent },
+  { path: 'user/:id', component: UserDetailComponent },
+  { path: 'cars', component: CarListComponent },
+  { path: 'car/:id', component: CarDetailComponent },
+  { path: 'bookings', component: BookingListComponent },
+  { path: 'booking/:id', component: BookingDetailComponent }
+];
