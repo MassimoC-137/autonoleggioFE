8cbd78646723166b45ecf84e2f85b833b74bfa94
MassimoC-137 <massimo.caramanna@gmail.com>
2024-07-10 16:26:25 +0200

    creazione component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon'
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule, 
+    MatFormFieldModule, 
+    MatIconModule, 
+    ReactiveFormsModule,
+    MatInputModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
 
+  protected loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value)
+    }
+  }
 }
